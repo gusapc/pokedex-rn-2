ba701122b3a5dcf523c5a9985a768e8af9c260e6
guapc <apcgus@gmail.com>
2020-08-21 15:50:28 -0500

    TeamScreen

--- app/screens/TeamScreen/TeamScreen.js
+++ app/screens/TeamScreen/TeamScreen.js
@@ -1,17 +1,27 @@
-import React /*{useEffect, useState}*/ from 'react';
-import { Text, View, Button } from 'react-native';
+import React from 'react';
+import { TouchableOpacity, View, ScrollView } from 'react-native';
 
 import styles from './TeamScreenStyle';
+import { useGenericObj } from 'pokedex-rn-2/app/hooks';
+import { Divider, PokeItem } from 'pokedex-rn-2/app/components';
 
-export default function TeamScreen (props) {
+export default function TeamScreen(props) {
+	const [teamInfo, teamLoader] = useGenericObj('TEAM', 'fetchCurrentUserTeam');
 	return (
-		<View style={styles.container}>
-			<View style={{ height: 100, width: 100 }} />
-			<Text>TeamScreen</Text>
+		<View style={[styles.containerWhite, styles.flex1]}>
+			<ScrollView>
+				{teamInfo.data.team.map((item, index, list) => (
+					<React.Fragment key={String(index)}>
+						<TouchableOpacity
+							onPress={() => props.navigation.navigate('PokemonDetailsScreen', { ...item })}
+							style={[styles.baseHorizontalPadding, styles.smallVerticalPadding, { height: 100 }]}
+						>
+							<PokeItem index={item.index} name={item.name} />
+						</TouchableOpacity>
+						{index !== list.length - 1 && <Divider addHorizontalMargin />}
+					</React.Fragment>
+				))}
+			</ScrollView>
 		</View>
 	);
 }
-
-
-
-
