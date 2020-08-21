19818c683f77d327629849d877961805365e7b2f
guapc <apcgus@gmail.com>
2020-08-21 14:22:22 -0500

    trainer profile

--- app/screens/TrainerProfileScreen/TrainerProfileScreen.js
+++ app/screens/TrainerProfileScreen/TrainerProfileScreen.js
@@ -1,17 +1,56 @@
-import React /*{useEffect, useState}*/ from 'react';
-import { Text, View, Button } from 'react-native';
-
+import React, { useState } from 'react';
+import { View, ScrollView, TouchableOpacity, Image } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 import styles from './TrainerProfileScreenStyle';
+import { TextComponent, HeaderNavbar, Divider, PokeItem } from 'pokedex-rn-2/app/components';
+import gball from 'pokedex-rn-2/assets/gball.png';
+import loading from 'pokedex-rn-2/assets/loading.jpg';
 
-export default function TrainerProfileScreen (props) {
+export default function TrainerProfileScreen(props) {
+	const displayName = props.navigation.getParam('displayName', '');
+	const photoURL = props.navigation.getParam('photoURL', '');
+	const team = props.navigation.getParam('team', '');
+
+	const [imgSrc, setImgSrc] = useState(
+		photoURL
+			? {
+					uri: photoURL,
+			  }
+			: gball,
+	);
 	return (
-		<View style={styles.container}>
-			<View style={{ height: 100, width: 100 }} />
-			<Text>TrainerProfileScreen</Text>
+		<View style={styles.containerWhite}>
+			<HeaderNavbar
+				left={
+					<Feather onPress={() => props.navigation.goBack()} name={'chevron-left'} size={35} color="black" />
+				}
+				right={<React.Fragment />}
+				center={<TextComponent color={'black'} weight={'bold'} size="title" align="center" text="Perfil de" />}
+			/>
+			<ScrollView>
+				<TextComponent color={'balck'} weight={'bold'} size="huge" align="center" text={displayName} />
+				<View style={[styles.row, styles.flex1, styles.justifyContentCenter]}>
+					<Image
+						style={styles.img}
+						source={imgSrc}
+						onError={() => {
+							setImgSrc(gball);
+						}}
+						defaultSource={loading}
+					/>
+				</View>
+				{team.map((item, index, list) => (
+					<React.Fragment key={String(index)}>
+						<TouchableOpacity
+							onPress={() => props.navigation.navigate('PokemonDetailsScreen', { ...item })}
+							style={[styles.baseHorizontalPadding, styles.smallVerticalPadding]}
+						>
+							<PokeItem index={item.index} name={item.name} />
+						</TouchableOpacity>
+						{index !== list.length - 1 && <Divider addHorizontalMargin />}
+					</React.Fragment>
+				))}
+			</ScrollView>
 		</View>
 	);
 }
-
-
-
-
--- app/screens/TrainerProfileScreen/TrainerProfileScreenStyle.js
+++ app/screens/TrainerProfileScreen/TrainerProfileScreenStyle.js
@@ -1,9 +1,16 @@
 import { StyleSheet } from 'react-native';
-// import { Fonts, Colors, Metrics } from 'pokedex-rn-2/app/styles';
+import { Fonts, Colors, Metrics } from 'pokedex-rn-2/app/styles';
 import { ApplicationStyles } from 'pokedex-rn-2/app/styles';
 
 export default StyleSheet.create({
-	 ...ApplicationStyles.screen,
-	// ...ApplicationStyles.flexBox,
-	// ...ApplicationStyles.margins,
+	...ApplicationStyles.screen,
+	...ApplicationStyles.flexBox,
+	...ApplicationStyles.margins,
+	img: {
+		width: Metrics.screenWidth / 2,
+		height: Metrics.screenWidth / 2,
+		borderRadius: 100,
+		borderWidth: 2,
+		borderColor: 'black',
+	},
 });
