830b2d351b5f3dfaf7398fe761d26151cf971781
guapc <apcgus@gmail.com>
2020-08-21 16:30:04 -0500

    hot fix empty team

--- app/screens/TeamScreen/TeamScreen.js
+++ app/screens/TeamScreen/TeamScreen.js
@@ -3,24 +3,30 @@ import { TouchableOpacity, View, ScrollView } from 'react-native';
 
 import styles from './TeamScreenStyle';
 import { useGenericObj } from 'pokedex-rn-2/app/hooks';
-import { Divider, PokeItem } from 'pokedex-rn-2/app/components';
+import { Divider, PokeItem, TextComponent } from 'pokedex-rn-2/app/components';
 
 export default function TeamScreen(props) {
 	const [teamInfo, teamLoader] = useGenericObj('TEAM', 'fetchCurrentUserTeam');
 	return (
 		<View style={[styles.containerWhite, styles.flex1]}>
 			<ScrollView>
-				{teamInfo.data.team.map((item, index, list) => (
-					<React.Fragment key={String(index)}>
-						<TouchableOpacity
-							onPress={() => props.navigation.navigate('PokemonDetailsScreen', { ...item })}
-							style={[styles.baseHorizontalPadding, styles.smallVerticalPadding, { height: 100 }]}
-						>
-							<PokeItem index={item.index} name={item.name} />
-						</TouchableOpacity>
-						{index !== list.length - 1 && <Divider addHorizontalMargin />}
-					</React.Fragment>
-				))}
+				{teamInfo.data.team ? (
+					teamInfo.data.team.map((item, index, list) => (
+						<React.Fragment key={String(index)}>
+							<TouchableOpacity
+								onPress={() => props.navigation.navigate('PokemonDetailsScreen', { ...item })}
+								style={[styles.baseHorizontalPadding, styles.smallVerticalPadding, { height: 100 }]}
+							>
+								<PokeItem index={item.index} name={item.name} />
+							</TouchableOpacity>
+							{index !== list.length - 1 && <Divider addHorizontalMargin />}
+						</React.Fragment>
+					))
+				) : (
+					<View style={styles.doubleVerticalMargin}>
+						<TextComponent align="center" text="Agrega algun Pokémon a tu equipo" />
+					</View>
+				)}
 			</ScrollView>
 		</View>
 	);
