64bafd4746b4f9e6e2076a7e05f22b073b5e8db8
Gus apc <apcgus@gmail.com>
2021-05-13 20:29:12 -0500

    expo 41

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
-import { 
-	StyleSheet, 
-	// Text, 
-	View 
+import React, { useState } from 'react';
+import {
+	StyleSheet,
+	View
 } from 'react-native';
 import { Provider } from 'react-redux';
 import store from 'pokedex-rn-2/app/Store.js';
@@ -11,52 +10,52 @@ import 'pokedex-rn-2/config/ReactotronConfig';
 import AppNavigator from './app/Router';
 import 'pokedex-rn-2/config/fixtimerbug';
 import { ContextProvider } from 'pokedex-rn-2/app/Context.js';
+import * as Font from 'expo-font';
+import mainBg from 'pokedex-rn-2/assets/mainBg.png';
+import gball from 'pokedex-rn-2/assets/gball.png';
+import loading from 'pokedex-rn-2/assets/loading.webp';
+import PokeIcon from 'pokedex-rn-2/assets/PokeIcon.png';
+import { Feather } from '@expo/vector-icons';
+import AppLoading from 'expo-app-loading';
+import { Asset } from 'expo-asset';
 
-// import * as Font from 'expo-font';
-// import mainBg from 'pokedex-rn-2/assets/mainBg.png';
-// import gball from 'pokedex-rn-2/assets/gball.png';
-// import loading from 'pokedex-rn-2/assets/loading.webp';
-// import PokeIcon from 'pokedex-rn-2/assets/PokeIcon.png';
-// import { Feather } from '@expo/vector-icons';
-// import { AppLoading } from 'expo';
-// import { Asset } from 'expo-asset';
-// function cacheFonts(fonts) {
-// 	return fonts.map((font) => Font.loadAsync(font));
-// }
+function cacheFonts(fonts) {
+	return fonts.map((font) => Font.loadAsync(font));
+}
 
-export default class App extends Component {
-	constructor(props) {
-		super(props);
+export default function App() {
+	const [isReady, setIsReady] = useState(false)
+	const cacheResourcesAsync = () => {
+		const images = [mainBg, PokeIcon, loading, gball];
+		const cacheImages = images.map((image) => {
+			return Asset.fromModule(image).downloadAsync();
+		});
+		const fontAssets = cacheFonts([Feather.font]);
+		return Promise.all([...cacheImages, ...fontAssets]);
 	}
-
-	// state = {
-	// 	isReady: false,
-	// };
-
-	// async cacheResourcesAsync() {
-	// 	const images = [mainBg, PokeIcon, loading, gball];
-	// 	const cacheImages = images.map((image) => {
-	// 		return Asset.fromModule(image).downloadAsync();
-	// 	});
-	// 	const fontAssets = cacheFonts([Feather.font]);
-	// 	return Promise.all([...cacheImages, ...fontAssets]);
-	// }
-	render() {
-		
+	if (isReady) {
 		return (
-			<Provider store={store}>
-				<ContextProvider>
-					<View style={styles.container}>
-						<AppNavigator
-							ref={(navigatorRef) => {
-								NavigationService.setTopLevelNavigator(navigatorRef);
-							}}
-						/>
-					</View>
-				</ContextProvider>
-			</Provider>
+			<AppLoading
+				startAsync={cacheResourcesAsync}
+				onFinish={() => setIsReady(true)}
+				onError={console.warn}
+			/>
 		);
 	}
+
+	return (
+		<Provider store={store}>
+			<ContextProvider>
+				<View style={styles.container}>
+					<AppNavigator
+						ref={(navigatorRef) => {
+							NavigationService.setTopLevelNavigator(navigatorRef);
+						}}
+					/>
+				</View>
+			</ContextProvider>
+		</Provider>
+	)
 }
 
 const styles = StyleSheet.create({
